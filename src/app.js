284a0cdd40b9b71b3b9851b4598a7abe0f6f3df5
Ramiro Batista da Luz <ramiroluz@gmail.com>
2020-08-03 19:41:07 -0300

    Delete a repository. fix #11, #12.

--- src/app.js
+++ src/app.js
@@ -55,8 +55,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-  return response.json({});
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+      return response.status(400).json({error: 'Repository not found!!'});
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
